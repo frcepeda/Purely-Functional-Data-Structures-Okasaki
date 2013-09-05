0f30e61e2c8c9f1550a83318a4990851b7e2ddde
Freddy Román <me@freddy.mx>
2013-09-04 22:31:01 -0500

    Renamed find to lookup on ex2.5.hs.

--- Chapter 2/ex2.5.hs
+++ Chapter 2/ex2.5.hs
@@ -3,7 +3,7 @@
 class FiniteMap m k a where
     empty :: m k a
     bind :: k -> a -> m k a -> m k a
-    find :: k -> m k a -> Maybe a
+    lookup :: k -> m k a -> Maybe a
     
 data TMap k a = E
               | TMap (k,a) (TMap k a) (TMap k a)
@@ -12,12 +12,12 @@ data TMap k a = E
 instance Ord k => FiniteMap TMap k a where
     empty = E
 
-    find k E = Nothing
-    find k t = find' t k t
-        where find' (TMap (k,a) _ _) x E = if k == x then Just a else Nothing
-              find' t x t'@(TMap (k,a) l r)
-                   | x <= k = find' t' x l
-                   | x > k  = find' t x r
+    lookup k E = Nothing
+    lookup k t = lookup' t k t
+        where lookup' (TMap (k,a) _ _) x E = if k == x then Just a else Nothing
+              lookup' t x t'@(TMap (k,a) l r)
+                   | x <= k = lookup' t' x l
+                   | x > k  = lookup' t x r
 
     bind k a E = TMap (k,a) E E
     bind k a t@(TMap (k',a') l r)
